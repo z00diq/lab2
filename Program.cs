1496027fac500f2fae7708f3b28636346ab70228
z00diq <45630610+z00diq@users.noreply.github.com>
2020-06-11 18:13:53 +0300

    1

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba2ISP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string culinf;
+            DateTime months = new DateTime();
+            culinf = Console.ReadLine();
+            for (int i = 0; i < 12; i++)
+            {
+                Console.WriteLine(months.ToString("MMMM", new System.Globalization.CultureInfo(culinf)));
+                months = months.AddMonths(1);
+            }
+
+
+
+            Random rand = new Random();
+            string s = Console.ReadLine();
+            var shuffle = string.Join("", s.OrderBy(x => rand.Next()));
+            Console.WriteLine(shuffle);
+
+
+
+            string s1 = Console.ReadLine();
+            foreach (char c in s1)
+            {
+                if (char.IsUpper(c)==true && !(c >= 'A' && c <= 'Z'))
+                Console.Write(c);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
